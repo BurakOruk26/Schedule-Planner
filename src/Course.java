5f7af4973999dc79c259ff2c240dd9af38079f76
Burak Oruk <91758706+BurakOruk26@users.noreply.github.com>
2022-08-06 21:29:43 +0300

    works that already been done
    I've done most of the basic stuff required for the programs back-end part before creating this repository on GitHub.
    Those files are uploaded.
    Includes two classes with their Javadocs: Lesson.java and Course.java.

---
+++ src/Course.java
@@ -0,0 +1,78 @@
+/**
+ * To imitate the schedule of a real life course this class will contain multiple "Lesson" class instances,
+ * stored in an ArrayList.
+ * @author Burak Oruk
+ * starting date: 06.08.2022
+ */
+
+import java.util.ArrayList;
+
+public class Course {
+
+    // variables
+    private String name;
+    private String instructor;
+    private int section;
+    private ArrayList<Lesson> lessons;
+
+    public Course( String name, String instructor, int section ){
+         this.name = name;
+         this.instructor = instructor;
+         this.section = section;
+
+         this.lessons = new ArrayList<Lesson>();
+    }
+
+    public boolean doesConflict( Course course ){
+        ArrayList<Lesson> otherLessons = course.lessons;
+
+        for ( Lesson lesson: this.lessons ){
+            for ( Lesson otherLesson: otherLessons ){
+                if ( lesson.equals(otherLesson) )
+                    return true;
+            }
+        }
+        return false;
+    }
+
+    // getter methods
+    public ArrayList<Lesson> getLessons() {
+        return lessons;
+    }
+    public int getSection() {
+        return section;
+    }
+    public String getInstructor() {
+        return instructor;
+    }
+    public String getName() {
+        return name;
+    }
+
+    // setter methods
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setInstructor(String instructor) {
+        this.instructor = instructor;
+    }
+    public void setSection(int section) {
+        this.section = section;
+    }
+    public void setLessons(ArrayList<Lesson> lessons) {
+        this.lessons = lessons;
+    }
+
+    public void addLesson( Lesson lesson ){
+        lessons.add(lesson);
+    }
+
+    public boolean removeLesson ( Lesson lesson ){
+        if ( lessons.contains( lesson ) ) {
+            lessons.remove(lesson);
+            return true;
+        }
+        else
+            return false;
+    }
+}
---
+++ src/Lesson.java
@@ -0,0 +1,67 @@
+/**
+ * This class stores when a lesson is going to happen and has a boolean "active" to make the job of "Planner" class easier.
+ * The class "Course" will use multiple instances of class "Lesson" to store data of when the courses will happen.
+ * @author Burak Oruk
+ * starting date: 06.08.2022
+ */
+public class Lesson {
+
+    // constants for days
+    public static final int MONDAY = 0;
+    public static final int TUESDAY = 1;
+    public static final int WEDNESDAY = 2;
+    public static final int THURSDAY = 3;
+    public static final int FRIDAY = 4;
+    public static final int SATURDAY = 5;
+    public static final int SUNDAY = 6;
+
+    // constants for lesson times (uses 12-hour clock system)
+    public static final int EIGHT_THIRTY = 0;
+    public static final int NINE_THIRTY = 1;
+    public static final int TEN_THIRTY = 2;
+    public static final int ELEVEN_THIRTY = 3;
+    public static final int TWELVE_THIRTY = 4;
+    public static final int ONE_THIRTY = 5;
+    public static final int TWO_THIRTY = 6;
+    public static final int THREE_THIRTY = 7;
+    public static final int FOUR_THIRTY = 8;
+    public static final int FIVE_THIRTY = 9;
+    public static final int SIX_THIRTY = 10;
+
+    // variables
+    private int day;
+    private int time;
+    private boolean active;
+
+    public Lesson( int day, int time ){
+        this.day = day;
+        this.time = time;
+        this.active = false; //this is false by default to prevent unnecessary clashes
+    }
+
+    public boolean equals( Lesson lesson ){
+        return ( (this.day == lesson.day) && (this.time == lesson.time) );
+    }
+
+    // setter methods
+    public void setDay(int day) {
+        this.day = day;
+    }
+    public void setTime(int time) {
+        this.time = time;
+    }
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    // getter methods
+    public int getDay() {
+        return day;
+    }
+    public int getTime() {
+        return time;
+    }
+    public boolean isActive() {
+        return active;
+    }
+}
